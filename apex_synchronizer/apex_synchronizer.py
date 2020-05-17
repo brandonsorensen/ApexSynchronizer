45bd2df2a0db14be9ada863fe3646ba5642e86d0
Brandon Sorensen <sorensen.12@gmail.com>
2020-05-16 21:43:22 -0600

    Created ApexSession class and concept

--- apex_synchronizer/apex_synchronizer.py
+++ apex_synchronizer/apex_synchronizer.py
@@ -1,69 +1,8 @@
-import os
-import requests
-from requests.auth import HTTPBasicAuth
-from datetime import datetime, timedelta
-from .utils import BASE_URL
+from .apex_session import ApexSession
 
 
 class ApexSynchronizer(object):
 
     def __init__(self):
-        self._access_token = ApexAccessToken.get_new_token()
+        self.session = ApexSession()
 
-    @property
-    def access_token(self):
-        if self._access_token.expiration < datetime.now():
-            self._access_token = ApexAccessToken.get_new_token()
-        return self._access_token
-
-    def get_header(self, custom_args: dict = None) -> dict:
-        header = {
-            'Authorization': f'Bearer {token}',
-            'Accept': 'application/json'
-        }
-
-        header.update(custom_args)
-        return header
-
-
-class ApexAccessToken(object):
-
-    PADDING = 10  # token will expire this many seconds before real expiration
-
-    def __init__(self, token_reponse):
-        as_json = token_reponse.json()
-        self.token = as_json['access_token']
-
-        expires_in = int(as_json['expire_in'])
-        # subtracting `PADDING` seconds to give some leeway
-        self.expiration = datetime.now() + timedelta(seconds=expires_in - ApexAccessToken.PADDING)
-        
-    @classmethod
-    def get_new_token(cls):
-        try:
-            client_id = os.environ['CONSUMER_KEY']
-            secret_key = os.environ['SECRET_KEY']
-        except KeyError:
-            raise EnvironmentError('ClientID or secret key are not in the environment.')
-
-        url = BASE_URL + 'token'
-        request_json = {
-                'grant_type': 'client_credentials',
-                'client_id': client_id,
-                'client_secret':secret_key 
-        }
-
-        headers = {"Accept": "application/json"}
-        auth = HTTPBasicAuth(client_id, secret_key)
-        r = requests.post(url, json=request_json, headers=headers, auth=auth)
-        if r.status_code == 200:
-            return cls(r)
-
-        # TODO: Add proper exception handling
-        raise Exception('Could not get token')
-
-    def __str__(self):
-        return self.token
-
-    def __repr__(self):
-        return f'ApexAccessToken({self.token})'
--- apex_synchronizer/utils.py
+++ apex_synchronizer/utils.py
@@ -1,1 +1,11 @@
 BASE_URL = 'https://api.apexvs.com/'
+
+
+def get_header(token, custom_args: dict = None) -> dict:
+    header = {
+        'Authorization': f'Bearer {token}',
+        'Accept': 'application/json'
+    }
+
+    header.update(custom_args)
+    return header
