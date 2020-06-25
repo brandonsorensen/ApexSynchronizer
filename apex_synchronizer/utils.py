9b2d6be9fc902b919b1b4003f78f7e6025ac44a8
Brandon Sorensen <sorensen.12@gmail.com>
2020-06-24 19:06:02 -0600

    Added get_ps_token function

--- apex_synchronizer/utils.py
+++ apex_synchronizer/utils.py
@@ -1,4 +1,7 @@
+import os
 import re
+import requests
+from urllib.parse import urljoin
 
 
 BASE_URL = 'https://api.apexvs.com/'
@@ -13,6 +16,32 @@ def get_header(token, custom_args: dict = None) -> dict:
     if custom_args is not None:
         header.update(custom_args)
     return header
+
+
+def get_ps_token():
+    header = {
+        'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8'"
+    }
+    try:
+        client_id = os.environ['PS_CLIENT_ID']
+        client_secret = os.environ['PS_CLIENT_SECRET']
+        url = urljoin(os.environ['PS_URL'], '/oauth/access_token')
+    except ValueError:
+        raise EnvironmentError('PowerSchool credentials are not in the environment.')
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    try:
+        r = requests.post(url, headers=header, data=payload)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    return r.json()['access_token']
 
 
 def snake_to_camel(var):
