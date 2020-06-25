50e616cb85911ec843e22363540656d12bf63d10
Brandon Sorensen <sorensen.12@gmail.com>
2020-06-25 15:41:21 -0600

    Moved PS token function to ps_agent file

--- apex_synchronizer/ps_agent.py
+++ apex_synchronizer/ps_agent.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from urllib.parse import urljoin
-from .utils import get_ps_token, get_header
+from .utils import get_header
 
 
 def fetch_classrooms():
@@ -13,4 +13,38 @@ def fetch_classrooms():
 
     r = requests.post(url, headers=header, params=payload)
     return r.json()['record']
+
+
+def get_ps_token():
+    header = {
+        'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8'"
+    }
+    try:
+        client_id = os.environ['PS_CLIENT_ID']
+        client_secret = os.environ['PS_CLIENT_SECRET']
+        url = urljoin(os.environ['PS_URL'], '/oauth/access_token')
+    except ValueError:
+        raise EnvironmentError('PowerSchool credentials are not in the environment.')
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    try:
+        r = requests.post(url, headers=header, data=payload)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    return r.json()['access_token']
+
+
+course2program_code = {
+    616: 'Z8102253',
+    615: 'Z9830940',
+    501: 'Z3844077',
+    601: 'Z2227630'
+}
 
--- apex_synchronizer/utils.py
+++ apex_synchronizer/utils.py
@@ -18,32 +18,6 @@ def get_header(token, custom_args: dict = None) -> dict:
     return header
 
 
-def get_ps_token():
-    header = {
-        'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8'"
-    }
-    try:
-        client_id = os.environ['PS_CLIENT_ID']
-        client_secret = os.environ['PS_CLIENT_SECRET']
-        url = urljoin(os.environ['PS_URL'], '/oauth/access_token')
-    except ValueError:
-        raise EnvironmentError('PowerSchool credentials are not in the environment.')
-
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret
-    }
-
-    try:
-        r = requests.post(url, headers=header, data=payload)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
-    
-    return r.json()['access_token']
-
-
 def snake_to_camel(var):
     """Converts snake case to camel case."""
     return ''.join(ch.capitalize() or '_' for ch in var.split('_'))
