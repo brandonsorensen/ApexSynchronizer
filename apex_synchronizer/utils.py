e818152b9c801d4a79633775547d8d8b54ed0505
Brandon Sorensen <sorensen.12@gmail.com>
2020-06-30 09:00:04 -0600

    Added camel_to_snake function.

--- apex_synchronizer/utils.py
+++ apex_synchronizer/utils.py
@@ -1,10 +1,8 @@
-import os
 import re
-import requests
-from urllib.parse import urljoin
 
 
 BASE_URL = 'https://api.apexvs.com/'
+CAMEL_REG = re.compile(r'(?<!^)(?=[A-Z])')
 
 
 def get_header(token, custom_args: dict = None) -> dict:
@@ -18,6 +16,11 @@ def get_header(token, custom_args: dict = None) -> dict:
     return header
 
 
-def snake_to_camel(var):
-    """Converts snake case to camel case."""
+def snake_to_camel(var: str) -> str:
+    """Converts snake_case to CamelCase."""
     return ''.join(ch.capitalize() or '_' for ch in var.split('_'))
+
+
+def camel_to_snake(var: str) -> str:
+    """Converts CamelCase to snake_case"""
+    return re.sub(CAMEL_REG, '_', var).lower()
