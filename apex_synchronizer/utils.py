fa94ab080e97330fac9519bbd6bdd9ec8d75940a
Brandon Sorensen <sorensen.12@gmail.com>
2020-06-30 17:35:40 -0600

    Added Levenshtein distance.

--- apex_synchronizer/utils.py
+++ apex_synchronizer/utils.py
@@ -1,4 +1,5 @@
 import re
+from typing import Iterable, Sequence
 
 
 BASE_URL = 'https://api.apexvs.com/'
@@ -24,3 +25,41 @@ def snake_to_camel(var: str) -> str:
 def camel_to_snake(var: str) -> str:
     """Converts CamelCase to snake_case"""
     return re.sub(CAMEL_REG, '_', var).lower()
+
+
+def levenshtein_distance(s, t):
+    """
+        iterative_levenshtein(s, t) -> ldist
+        ldist is the Levenshtein distance between the strings
+        s and t.
+        For all i and j, dist[i,j] will contain the Levenshtein
+        distance between the first i characters of s and the
+        first j characters of t
+        Credit: https://www.python-course.eu/levenshtein_distance.php
+    """
+
+    rows = len(s) + 1
+    cols = len(t) + 1
+    dist = [[0 for x in range(cols)] for x in range(rows)]
+
+    # source prefixes can be transformed into empty strings
+    # by deletions:
+    for i in range(1, rows):
+        dist[i][0] = i
+
+    # target prefixes can be created from an empty source string
+    # by inserting the characters
+    for i in range(1, cols):
+        dist[0][i] = i
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row - 1] == t[col - 1]:
+                cost = 0
+            else:
+                cost = 1
+            dist[row][col] = min(dist[row - 1][col] + 1,  # deletion
+                                 dist[row][col - 1] + 1,  # insertion
+                                 dist[row - 1][col - 1] + cost)  # substitution
+
+    return dist[row][col]
