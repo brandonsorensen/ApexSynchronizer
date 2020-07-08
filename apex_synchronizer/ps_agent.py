bc0dd8b5afd47c0515cbc866bbd262fecd4c526b
Brandon Sorensen <sorensen.12@gmail.com>
2020-07-08 14:07:00 -0600

    Documented PSEnrollment methods.

--- apex_synchronizer/ps_agent.py
+++ apex_synchronizer/ps_agent.py
@@ -1,11 +1,20 @@
 import os
 import requests
 from collections import defaultdict
+from typing import Iterable, Set, Union
 from urllib.parse import urljoin
 from .utils import get_header, flatten_ps_json
 
 
 BASE_QUERY_URL = '/ws/schema/query/com.apex.learning.school.'
+
+
+course2program_code = {
+    616: 'Z8102253',
+    615: 'Z9830940',
+    501: 'Z3844077',
+    601: 'Z2227630'
+}
 
 
 class PSEnrollment(object):
@@ -14,45 +23,69 @@ class PSEnrollment(object):
         if ps_json is None:
             ps_json = fetch_enrollment()
 
-        json_obj = flatten_ps_json(ps_json)
+        json_obj = map(flatten_ps_json, ps_json)
         self._parse_json(json_obj)
 
-    def get_classrooms(self, eduid):
-        return self.student2classrooms[eduid]
+    def get_classrooms(self, eduid: Union[int, str]) -> Set[int]:
+        """
+        Returns all classrooms in which a given student is enrolled. Students
+        are indexed by their EDUIDs, which may be given as an int or a
+        numeric string.
 
-    def get_roster(self, section_id):
-        return self.classroom2students[section_id]
+        :param Union[int, str] eduid: the EDUID of a given student
+        :return: all classrooms in which the student is enrolled
+        :rtype: set[int]
+        """
+        return self.student2classrooms[int(eduid)]
 
-    def _parse_json(self, json_obj):
+    def get_roster(self, section_id: Union[int, str]) -> Set[int]:
+        """
+        Returns the roster of a given classroom, indexed by its section ID.
+        Section IDs may be given as integers or numeric strings.
+
+        :param Union[int, str] section_id: the section ID of the classroom
+        :return: the EDUIDs of all students in the classroom
+        :rtype: set[int]
+        """
+        return self.classroom2students[int(section_id)]
+
+    def _parse_json(self, json_obj: Iterable[dict]):
+        """Parses a JSON object returned by the `fetch_enrollment` function."""
         self.student2classrooms = defaultdict(set)
         self.classroom2students = defaultdict(set)
 
-        for sec_id, eduid in json_obj:
+        for entry in json_obj:
+            eduid = int(entry['eduid'])
+            sec_id = int(entry['section_id'])
+
             self.student2classrooms[eduid].add(sec_id)
             self.classroom2students[sec_id].add(eduid)
 
+        self.student2classrooms = dict(self.student2classrooms)
+        self.classroom2students = dict(self.classroom2students)
+
 
 def fetch_classrooms() -> dict:
-    return _fetch_powerquery(BASE_QUERY_URL + 'classrooms')
+    return _fetch_powerquery('classrooms')
 
 
 def fetch_staff() -> dict:
-    return _fetch_powerquery(BASE_QUERY_URL + 'teachers')
+    return _fetch_powerquery('teachers')
 
 
 def fetch_students() -> dict:
-    return _fetch_powerquery(BASE_QUERY_URL + 'students')
+    return _fetch_powerquery('students')
 
 
 def fetch_enrollment() -> dict:
-    return _fetch_powerquery(BASE_QUERY_URL + 'enrollment')
+    return _fetch_powerquery('enrollment')
 
 
-def _fetch_powerquery(url, page_size=0) -> dict:
+def _fetch_powerquery(url_ext: str, page_size=0) -> dict:
     token = get_ps_token()
     header = get_header(token, custom_args={'Content-Type': 'application/json'})
     payload = {'pagesize': page_size}
-    url = urljoin(os.environ['PS_URL'], url)
+    url = urljoin(os.environ['PS_URL'], BASE_QUERY_URL + url_ext)
 
     r = requests.post(url, headers=header, params=payload)
     return r.json()['record']
@@ -83,11 +116,4 @@ def get_ps_token() -> str:
     
     return r.json()['access_token']
 
-
-course2program_code = {
-    616: 'Z8102253',
-    615: 'Z9830940',
-    501: 'Z3844077',
-    601: 'Z2227630'
-}
 
