9ee7f36c321ca50a34375d71b8ab9ea46bfbd42d
Brandon Sorensen <sorensen.12@gmail.com>
2020-07-09 15:55:40 -0600

    Added enrollment module import.

--- apex_synchronizer/__init__.py
+++ apex_synchronizer/__init__.py
@@ -1,4 +1,5 @@
 from .apex_synchronizer import ApexSynchronizer
 from . import apex_data_models as adm
 from .apex_session import ApexSession
-from .import ps_agent
+from . import ps_agent
+from . import enrollment
