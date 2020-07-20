0c407bd428f57b91c6fd8dfa072cee825cf2377f
Brandon Sorensen <sorensen.12@gmail.com>
2020-07-20 17:32:07 -0600

    Imports get_classrooms_for_eduids.

--- apex_synchronizer/apex_data_models/__init__.py
+++ apex_synchronizer/apex_data_models/__init__.py
@@ -1,6 +1,5 @@
 from .apex_data_object import ApexDataObject
 from .apex_student import ApexStudent
-from .apex_classroom import ApexClassroom
+from .apex_classroom import ApexClassroom, get_classrooms_for_eduids
 from .apex_staff_member import ApexStaffMember
 from .utils import BASE_URL
-
