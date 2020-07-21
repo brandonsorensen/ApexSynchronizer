4ca951d06798d0b234cdbf13b2aa2d53cc6ca3db
Brandon Sorensen <sorensen.12@gmail.com>
2020-07-21 14:21:44 -0600

    Documentation update.

--- apex_synchronizer/ps_agent.py
+++ apex_synchronizer/ps_agent.py
@@ -1,3 +1,16 @@
+"""
+The module servers as an interface for querying the PowerSchool
+database. It defines a function obtaining an PowerSchool token
+from the environment and a function for using that token to fetch
+a query from a given URL. Additionally, there are four wrapper functions
+that simply call the `_fetch_powerquery` function for an available
+URL. In practice, these are the functions that compose the API.
+
+The `course2program_code`, aptly named, maps a course code as defined
+in the documentation provided by Apex Learning to their respective
+program codes.
+"""
+
 import logging
 import os
 from urllib.parse import urljoin
@@ -34,6 +47,17 @@ def fetch_enrollment() -> dict:
 
 
 def _fetch_powerquery(url_ext: str, page_size=0) -> dict:
+    """
+    Obtains an access token and calls a PowerQuery at a given url,
+    limiting it to `page_size` results.
+
+    :param url_ext: the extension that, appended to the `PS_URL`
+        environment variable and `BASE_URL` as defined above,
+        composes the URL
+    :param page_size: how many results to return, 0 = all
+    :raises PSEmptyQueryException: when no results are returned
+    :return: the JSON object returned by the PowerQuery
+    """
     logger = logging.getLogger(__name__)
     logger.info('Fetching PowerQuery with extension ' + str(url_ext))
     token = get_ps_token()
@@ -52,6 +76,16 @@ def _fetch_powerquery(url_ext: str, page_size=0) -> dict:
 
 
 def get_ps_token() -> str:
+    """
+    Gets the PowerSchool access token from the PowerSchool server using
+    the following environment variables:
+
+        - PS_CLIENT_ID: the given client ID for the PowerSchool plugin
+        - PS_CLIENT_SECRET: the secret code
+        - PS_URL: the PowerSchool URL
+
+    :return: an access token for the PowerSchool server
+    """
     header = {
         'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8'"
     }
