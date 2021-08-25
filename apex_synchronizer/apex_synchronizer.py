7f2f84337ed2a79b6d183bcbb328091c58a63b92
Brandon Sorensen <sorensen.12@gmail.com>
2021-08-25 11:52:49 -0600

    Broke classroom operations in separate class.

--- apex_synchronizer/apex_synchronizer.py
+++ apex_synchronizer/apex_synchronizer.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, KeysView
+from datetime import datetime
 from dataclasses import dataclass
 from os import environ
 from pathlib import Path
@@ -64,14 +65,14 @@ class ApexSynchronizer(object):
     def __init__(self, exclude=None):
         """Opens a session with the Apex API and initializes a logger."""
         self.session = ApexSession()
-        self._dry_run = bool(int(environ.get('APEX_DRY_RUN', False)))
-        if self._dry_run:
-            self._operations = {}
+        self.dry_run = bool(int(environ.get('APEX_DRY_RUN', False)))
+        self.operations = {}
         self.logger = logging.getLogger(__name__)
         self.batch_jobs = []
         self.ps_staff = {}
         self.apex_staff = set()
         self.apex_enroll, self.ps_enroll = self._init_enrollment(exclude)
+        self.sync_classrooms = ClassroomDelegate(self)
 
     def save(self):
         """
@@ -85,13 +86,15 @@ class ApexSynchronizer(object):
         during object deconstruction.
         """
         try:
-            if not self._dry_run:
+            if not self.dry_run:
                 return
         except AttributeError:
             return
 
         with open('dry_run_info.json', 'w+') as f:
-            json.dump(self._operations, f)
+            self.operations['runtime'] = (datetime.now()
+                                          .strftime('%Y-%m-%d %H:%M:%S %Z'))
+            json.dump(self.operations, f)
 
     @property
     def apex_roster(self) -> KeysView:
@@ -116,14 +119,14 @@ class ApexSynchronizer(object):
             self.logger.info('Rosters already in sync')
             return
 
-        if self._dry_run:
+        if self.dry_run:
             ops = {}
             if len(to_enroll) > 0:
                 ops['to_enroll'] = list(to_enroll)
             if len(to_withdraw) > 0:
                 ops['to_withdraw'] = list(to_withdraw)
             if len(ops) > 0:
-                self._operations['sync_roster'] = ops
+                self.operations['sync_roster'] = ops
             return
 
         if len(to_enroll) > 0:
@@ -157,8 +160,8 @@ class ApexSynchronizer(object):
             self.logger.info('Staff list in sync.')
             return
 
-        if self._dry_run:
-            self._operations['sync_staff'] = {
+        if self.dry_run:
+            self.operations['sync_staff'] = {
                 'to_post': list(post_ids)
             }
 
@@ -219,16 +222,16 @@ class ApexSynchronizer(object):
 
                 apex_to_enroll = [self.apex_enroll.get_student_for_id(id_)
                                   for id_ in to_enroll]
-                if self._dry_run:
+                if self.dry_run:
                     # TODO: Make this a defaultdict
                     try:
-                        op = self._operations['sync_classroom_enrollment']
+                        op = self.operations['sync_classroom_enrollment']
                         if 'to_enroll' in op.keys():
                             op['to_enroll'][c_id] = list(to_enroll)
                         else:
                             op['to_enroll'] = {c_id: list(to_enroll)}
                     except KeyError:
-                        self._operations['sync_classroom_enrollment'] = {
+                        self.operations['sync_classroom_enrollment'] = {
                             'to_enroll': {c_id: list(to_enroll)}
                         }
                     n_updates = 0
@@ -237,15 +240,15 @@ class ApexSynchronizer(object):
                                                       classroom=apex_classroom)
                 n_entries_changed += n_updates
 
-            if to_withdraw and self._dry_run:
+            if to_withdraw and self.dry_run:
                 try:
-                    op = self._operations['sync_classroom_enrollment']
+                    op = self.operations['sync_classroom_enrollment']
                     if 'to_withdraw' in op.keys():
                         op['to_withdraw'][c_id] = list(to_withdraw)
                     else:
                         op['to_withdraw'] = {c_id: list(to_withdraw)}
                 except KeyError:
-                    self._operations['sync_classroom_enrollment'] = {
+                    self.operations['sync_classroom_enrollment'] = {
                         'to_withdraw': {c_id: list(to_withdraw)}
                     }
                 n_withdrawn = 0
@@ -254,112 +257,6 @@ class ApexSynchronizer(object):
                                                          to_withdraw=to_withdraw)
             n_entries_changed += n_withdrawn
         self.logger.info(f'Updated {n_entries_changed} enrollment records.')
-
-    def sync_classrooms(self):
-        """
-        Ensures that all relevant classrooms that are present in
-        PowerSchool appear in the Apex Learning database.
-        """
-        total = 0
-        updated = 0
-        to_post = []
-        class_ops = defaultdict(list)
-        for i, (section, progress) in enumerate(walk_ps_sections(archived=False,
-                                                                 filter_date=False)):
-            try:
-                """
-                This will get checked again below, but if we can
-                rule a section out before making a GET call to the
-                Apex server, it saves an appreciable amount of time.
-                """
-                if not section['apex_program_code']:
-                    self.logger.info(f'{progress}:Section {section["section_id"]} '
-                                     'has no program codes. Skipping...')
-                    total += 1
-                    continue
-            except KeyError:
-                raise exceptions.ApexMalformedJsonException(section)
-            try:
-                section_id = section['section_id']
-                if not section_id:
-                    self.logger.info('No classroom ID give for object below. '
-                                     'Skipping.\n' + str(section))
-                self.logger.info(f'{progress}:Attempting to fetch classroom with'
-                                 f' ID {section_id}.')
-                apex_cr = ApexClassroom.get(section_id, session=self.session)
-                ps_cr = ApexClassroom.from_powerschool(section,
-                                                       already_flat=True)
-                self.logger.info(f'{progress}:Classroom found.')
-                if apex_cr != ps_cr:
-                    if set(ps_cr.product_codes) <= set(apex_cr.product_codes):
-                        # when Apex contains more than just the PS code
-                        ps_cr.product_codes = apex_cr.product_codes
-                        if apex_cr == ps_cr:
-                            continue
-                    self.logger.info('Updating record '
-                                     + str(ps_cr.import_classroom_id))
-                    if self._dry_run:
-                        class_ops['to_update'].append(ps_cr.import_classroom_id)
-                    else:
-                        r = ps_cr.put_to_apex(session=self.session)
-                        try:
-                            r.raise_for_status()
-                            self.logger.debug('Updated classrooms with ID '
-                                              f'{ps_cr.import_classroom_id}.')
-                            apex_cr.update(ps_cr, session=self.session)
-                            self.apex_enroll.update_classroom(ps_cr)
-                        except requests.exceptions.HTTPError:
-                            self.logger.exception('Received bad response: '
-                                                  + str(r.status_code))
-                    updated += 1
-            except KeyError:
-                raise exceptions.ApexMalformedJsonException(section)
-            except exceptions.ApexObjectNotFoundException:
-                self.logger.info(f'{progress}:Classroom not found in Apex. '
-                                 'Creating classroom')
-                try:
-                    apex_obj = ApexClassroom.from_powerschool(section,
-                                                              already_flat=True)
-                    to_post.append(apex_obj)
-                except exceptions.NoProductCodesException as e:
-                    self.logger.info(e)
-            except (exceptions.ApexNotAuthorizedError,
-                    exceptions.ApexConnectionException):
-                self.logger.exception('Failed to connect to Apex server.')
-                return
-            except exceptions.ApexError:
-                self.logger.exception('Encountered unexpected error:\n')
-            finally:
-                total += 1
-
-        if self._dry_run and len(to_post) > 0:
-            ops = []
-            for cr in to_post:
-                as_dict = cr.to_dict()
-                as_dict['classroom_start_date'] = str(as_dict['classroom_start_date'])
-                ops.append(as_dict)
-
-            class_ops['to_post'] = ops
-
-        self.logger.info(f'Updated {updated} classrooms.')
-        self.logger.info(f'Posting {len(to_post)} classrooms.')
-        r = ApexClassroom.post_batch(to_post, session=self.session)
-        # TODO: Parse response messages
-        try:
-            r.raise_for_status()
-            self.logger.info(f'Added {len(to_post)}/{total} classrooms.')
-        except requests.exceptions.HTTPError:
-            errors = adm.apex_classroom.handle_400_response(r, self.logger)
-            n_posted = len(to_post) - len(errors)
-            self.logger.info(f'Received {len(errors)} errors:\n'
-                             + str(errors))
-            if n_posted:
-                self.logger.info(f'Successfully added {n_posted} classrooms.')
-            else:
-                self.logger.info('No classrooms were added to Apex.')
-
-        if self._dry_run:
-            self._operations['sync_classrooms'] = dict(class_ops)
 
     def run_schedule(self, s: ApexSchedule):
         """
@@ -454,10 +351,10 @@ class ApexSynchronizer(object):
                 st.update_apex()
                 to_update.append(st.powerschool)
 
-        if self._dry_run and change_log:
-            if 'sync_roster' not in self._operations.keys():
-                self._operations['sync_roster'] = {}
-            self._operations['sync_roster']['to_update'] = change_log
+        if self.dry_run and change_log:
+            if 'sync_roster' not in self.operations.keys():
+                self.operations['sync_roster'] = {}
+            self.operations['sync_roster']['to_update'] = change_log
 
         return to_update
 
@@ -698,3 +595,202 @@ class ApexSynchronizer(object):
             self.logger.info(f'Successfully withdrew {n_withdrawn}/'
                              f'{len(to_withdraw)} students from {c_id}.')
         return n_withdrawn
+
+
+class ClassroomDelegate(object):
+
+    def __init__(self, synchronizer: ApexSynchronizer):
+        self.sync = synchronizer
+        self.logger = self.sync.logger  # For convenience
+
+        self.class_ops = defaultdict(list)
+        self.total = self.updated = 0
+        self.to_post = []
+        self.progress = ''
+
+    def __call__(self, *args, **kwargs):
+        self.sync_classrooms()
+
+    def add_to_post(self, section: dict):
+        """
+        Adds sections that do not exist in Apex to its database.
+        :param section:
+        :return:
+        """
+        self.logger.info(f'{self.progress}:Classroom not found in Apex. '
+                         'Creating classroom.')
+        try:
+            apex_obj = ApexClassroom.from_powerschool(section,
+                                                      already_flat=True)
+            self.to_post.append(apex_obj)
+        except exceptions.NoProductCodesException as e:
+            self.logger.debug(e)
+
+    @staticmethod
+    def classrooms_equal(apex: ApexClassroom, powerschool: ApexClassroom) -> bool:
+        """
+        Determines whether two `ApexClassroom` objects. This method is
+        necessary in addition to the the `ApexClassroom` class's magic
+        method because classrooms in Apex's database can contain more
+        than one product code, while PowerSchool only permits one. For
+        the purposes of the sync, any `ApexClassroom` object whose
+        product codes intersection with that of a PowerSchool
+        counterpart will be considered equal, provided all other
+        information matches.
+
+        Due to limitations in Apex's API, it is not possible to remove
+        product codes, so this extra step is necessary.
+
+        :param apex: the `ApexClassroom` object from the Apex database
+        :param powerschool: the `ApexClassroom` object from PowerSchool
+        :return: whether the two classrooms are equal
+        """
+        if apex != powerschool:
+            if set(powerschool.product_codes) <= set(apex.product_codes):
+                # when Apex contains more than just the PS code
+                powerschool.product_codes = apex.product_codes
+                return apex == powerschool
+            return False
+        return True
+
+    def get_classrooms(self, section: dict) \
+            -> Tuple[ApexClassroom, ApexClassroom]:
+        """
+        Creates `ApexClassroom` objects for the Apex and PowerSchool
+        sections.
+
+        :param section: the relevant section as a `dict` object.
+        :raises exception.ApexObjectNotFoundException: when the section
+                cannot be located in the Apex database
+        :return: a tuple containing two `ApexClassroom` objects.
+        """
+        section_id = section['section_id']
+        if not section_id:
+            self.logger.info('No classroom ID give for object below. '
+                             'Skipping.\n' + str(section))
+        self.logger.info(f'{self.progress}:Attempting to fetch classroom with'
+                         f' ID {section_id}.')
+        apex_cr = ApexClassroom.get(section_id, session=self.sync.session)
+        ps_cr = ApexClassroom.from_powerschool(section,
+                                               already_flat=True)
+        self.logger.info(f'{self.progress}:Classroom found.')
+        return apex_cr, ps_cr
+
+    def has_program_code(self, section: dict) -> bool:
+        """
+        Checks whether a section has an Apex program code.
+        :param section: the relevant section
+        :return: whether the section has a program code
+        """
+        try:
+            """
+            This will get checked again below, but if we can
+            rule a section out before making a GET call to the
+            Apex server, it saves an appreciable amount of time.
+            """
+            if not section['apex_program_code']:
+                self.logger.info(f'{self.progress}:Section {section["section_id"]} '
+                                 'has no program codes. Skipping...')
+                self.total += 1
+                return False
+            return True
+        except KeyError:
+            raise exceptions.ApexMalformedJsonException(section)
+
+    def log_post_operations(self):
+        """
+        Add the collected POST operations to the parent
+        `ApexSynchronizer` class's `operations` attribute.
+        """
+        if len(self.to_post) > 0:
+            ops = []
+            for cr in self.to_post:
+                as_dict = cr.to_dict()
+                as_dict['classroom_start_date'] = str(as_dict['classroom_start_date'])
+                ops.append(as_dict)
+
+            self.class_ops['to_post'] = ops
+
+    def post_classrooms(self):
+        """
+        POST the new classrooms to the Apex database and parse the
+        responses.
+        """
+        self.log_post_operations()
+        self.logger.info(f'Posting {len(self.to_post)} classrooms.')
+        r = ApexClassroom.post_batch(self.to_post, session=self.sync.session)
+        # TODO: Parse response messages
+        try:
+            r.raise_for_status()
+            self.logger.info(f'Added {len(self.to_post)}/{self.total} classrooms.')
+        except requests.exceptions.HTTPError:
+            errors = adm.apex_classroom.handle_400_response(r, self.logger)
+            n_posted = len(self.to_post) - len(errors)
+            self.logger.info(f'Received {len(errors)} errors:\n'
+                             + str(errors))
+            if n_posted:
+                self.logger.info(f'Successfully added {n_posted} classrooms.')
+            else:
+                self.logger.info('No classrooms were added to Apex.')
+
+    def sync_classrooms(self):
+        """
+        Ensures that all relevant classrooms that are present in
+        PowerSchool appear in the Apex Learning database.
+        """
+        for i, (section, progress) in enumerate(
+                walk_ps_sections(archived=False, filter_date=False)
+        ):
+            self.progress = progress
+            if not self.has_program_code(section):
+                continue
+            try:
+                # Check equality, update if inconsistent
+                apex_cr, ps_cr = self.get_classrooms(section)
+                if not self.classrooms_equal(apex_cr, ps_cr):
+                    self.logger.info('Updating record '
+                                     + str(ps_cr.import_classroom_id))
+                    self.class_ops['to_update'].append(
+                        ps_cr.import_classroom_id
+                    )
+                    if not self.sync.dry_run:
+                        self.update_classroom(apex_cr, ps_cr)
+                    self.updated += 1
+            except KeyError:
+                raise exceptions.ApexMalformedJsonException(section)
+            except exceptions.ApexObjectNotFoundException:
+                # Object ID not found in Apex database
+                self.add_to_post(section)
+            except (exceptions.ApexNotAuthorizedError,
+                    exceptions.ApexConnectionException):
+                self.logger.exception('Failed to connect to Apex server.')
+                return
+            except exceptions.ApexError:
+                self.logger.exception('Encountered unexpected error:\n')
+            finally:
+                self.total += 1
+        self.logger.info(f'Updated {self.updated} classrooms.')
+
+        if not self.sync.dry_run:
+            self.post_classrooms()
+        self.sync.operations['sync_classrooms'] = dict(self.class_ops)
+
+    def update_classroom(self, apex_cr: ApexClassroom,
+                         ps_cr: ApexClassroom):
+        """
+        Updates a classroom in the Apex database and the `ApexSynchronizer`
+        class's enrollment objects. (See the `enrollment` module.)
+
+        :param apex_cr: current Apex classroom
+        :param ps_cr: PowerSchool equivalent used to update the Apex DB
+        """
+        r = ps_cr.put_to_apex(session=self.sync.session)
+        try:
+            r.raise_for_status()
+            self.logger.debug('Updated classrooms with ID '
+                              f'{ps_cr.import_classroom_id}.')
+            apex_cr.update(ps_cr, session=self.sync.session)
+            self.sync.apex_enroll.update_classroom(ps_cr)
+        except requests.exceptions.HTTPError:
+            self.logger.exception('Received bad response: '
+                                  + str(r.status_code))
